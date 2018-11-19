e884946976feaf0d567fec789e2328468a6af722
GwonSeongJu <nayagwon@naver.com>
2018-11-20 01:50:10 +0900

    update index.js server file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+var path     = require('path');
+
+
+//view engine
+app.set('views', __dirname + '/views');
+app.engine('html',require('ejs').renderFile);
+
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/',function(req,res){
+    res.render('index.html');
+})
+
+app.listen(3000, function(){
+    console.log('Server On!');
+});
